15dfe9aa2fea8ba2147c5572fd4b5032eeb52604
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-12-17 01:21:53 +0530

    Fix and remove redundant lines

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const { error } = require("console");
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-  res.type("html").status(200);
   fs.readFile("./index.html", (err, data) => {
     if (err) throw err;
     return res.send(data);
@@ -13,7 +11,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.type("html").status(200);
   fs.readFile("./about.html", (err, data) => {
     if (err) throw err;
     return res.send(data);
@@ -21,7 +18,6 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/contact-me", (req, res) => {
-  res.type("html").status(200);
   fs.readFile("./contact-me.html", (err, data) => {
     if (err) throw err;
     return res.send(data);
@@ -29,10 +25,9 @@ app.get("/contact-me", (req, res) => {
 });
 
 app.all("*", (req, res) => {
-  res.type("html").status(404);
-  fs.readFile("404.html", (err, data) => {
+  fs.readFile("./404.html", (err, data) => {
     if (err) throw error;
-    return res.send(data);
+    return res.type("html").status(404).send(data);
   });
 });
 
